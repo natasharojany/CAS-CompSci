a06b774ca36d21ae52a1636016242b1bb4b31383
natasharojany <48220208+natasharojany@users.noreply.github.com>
2019-10-02 09:10:15 -0700

    Add files via upload

---
+++ 12 pics/12 pics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  12 pics
+//
+//  Created by Natasha Rojany on 10/2/19.
+//  Copyright © 2019 Natasha Rojany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
