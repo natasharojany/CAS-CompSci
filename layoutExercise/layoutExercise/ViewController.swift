b45ba7355aeaeac6189960753966ae097e50401b
natasharojany <48220208+natasharojany@users.noreply.github.com>
2020-02-02 20:23:47 -0800

    Add files via upload

---
+++ layoutExercise/layoutExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layoutExercise
+//
+//  Created by Natasha Rojany on 1/29/20.
+//  Copyright © 2020 Natasha Rojany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
